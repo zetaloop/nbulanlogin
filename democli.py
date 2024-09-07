edabdc5806cc446f3cd54a1aec96eedca19e10d9
zetaloop <zetaloop@outlook.com>
2024-09-08 05:19:22 +0800

    Some decorations

--- democli.py
+++ democli.py
@@ -8,10 +8,10 @@ def get_current_state():
     state = getstate()
     match state:
         case "Connected":
-            print("当前已登录")
+            print("[当前已登录]")
             return True, None
         case "Login":
-            print("当前未登录")
+            print("[当前未登录]")
             return False, state.more
         case "Unknown":
             raise ValueError(f"数据异常: {state.more[:400]}")
@@ -22,13 +22,13 @@ def get_current_state():
 
 
 def get_login_result(url, username, password):
-    print("正在登录...")
+    print(">>正在登录...")
     if username is None or password is None:
         raise ValueError("无法登录: 请指定用户名和密码")
     state = login(url, username, password)
     match state:
         case "Success":
-            print("登录成功")
+            print(">>登录成功!")
             return True  # Online
         case "Failed":
             assert isinstance(state, XStrType)
@@ -38,11 +38,11 @@ def get_login_result(url, username, password):
 
 
 def get_logout_result():
-    print("正在退出...")
+    print(">>正在退出...")
     state = logout()
     match state:
         case "Success":
-            print("退出成功")
+            print(">>退出成功!")
             return False  # Offline
         case "Failed":
             assert isinstance(state, XStrType)
@@ -94,7 +94,7 @@ def main():
                     try:
                         get_login_result(url, args.username, args.password)
                     except RuntimeError:
-                        print("3秒后重试...")
+                        print(">>3秒后重试...")
                         time.sleep(3)
                         get_login_result(url, args.username, args.password)
             else:
