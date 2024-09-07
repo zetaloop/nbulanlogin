6b324d1ac3f7c797c425e33a0aa764d7c2d2496e
zetaloop <zetaloop@outlook.com>
2024-09-08 05:14:55 +0800

    Refactor

--- democli.py
+++ democli.py
@@ -1,7 +1,54 @@
 import argparse
 import time
 import datetime
-from core import getstate, login, logout
+from core import getstate, login, logout, _xstr as XStrType
+
+
+def get_current_state():
+    state = getstate()
+    match state:
+        case "Connected":
+            print("当前已登录")
+            return True, None
+        case "Login":
+            print("当前未登录")
+            return False, state.more
+        case "Unknown":
+            raise ValueError(f"数据异常: {state.more[:400]}")
+        case "NetworkError":
+            raise ConnectionError("网络连接失败")
+        case never:
+            assert False
+
+
+def get_login_result(url, username, password):
+    print("正在登录...")
+    if username is None or password is None:
+        raise ValueError("无法登录: 请指定用户名和密码")
+    state = login(url, username, password)
+    match state:
+        case "Success":
+            print("登录成功")
+            return True  # Online
+        case "Failed":
+            assert isinstance(state, XStrType)
+            raise RuntimeError(f"登录失败: {state.more}")
+        case never:
+            assert False
+
+
+def get_logout_result():
+    print("正在退出...")
+    state = logout()
+    match state:
+        case "Success":
+            print("退出成功")
+            return False  # Offline
+        case "Failed":
+            assert isinstance(state, XStrType)
+            raise RuntimeError(f"退出失败: {state.more}")
+        case never:
+            assert False
 
 
 def main():
@@ -10,55 +57,48 @@ def main():
         "action",
         nargs="?",
         default="login",
-        choices=["login", "logout", "relogin"],
+        choices=["login", "logout", "relogin", "check"],
         help="the action to perform (default: %(default)s)",
     )
     parser.add_argument(
         "-u",
         "--username",
-        required=True,
         metavar="MyName",
         help="network account username",
     )
     parser.add_argument(
         "-p",
         "--password",
-        required=True,
         metavar="aBc123",
         help="network account password",
     )
     args = parser.parse_args()
 
-    if args.action == "logout":
-        logout()
-        print("已登出")
-    elif args.action == "relogin":
-        logout()
-        print("已登出，正在重新登录...")
-        time.sleep(3)
-        perform_login(args.username, args.password)
-    else:
-        perform_login(args.username, args.password)
-
-
-def perform_login(username, password):
-    state = getstate()
-    match state:
-        case "Connected":
-            print("当前已登录")
-        case "Login":
-            print("当前未登录")
-            match login(state.more, username, password):
-                case "Success":
-                    print("登录成功")
-                case "Failed":
-                    print(f"登录失败: {state.more}")
-                case "NetworkError":
-                    print("连接失败")
-        case "Unknown":
-            print(f"数据异常: {state.more[:400]}")
-        case "NetworkError":
-            print("网络连接失败")
+    match args.action:
+        case "check":
+            get_current_state()
+        case "login":
+            online, url = get_current_state()
+            if not online:
+                get_login_result(url, args.username, args.password)
+        case "logout":
+            online, _ = get_current_state()
+            if online:
+                get_logout_result()
+        case "relogin":
+            online, url = get_current_state()
+            if online:
+                get_logout_result()
+                online, url = get_current_state()
+                if not online:
+                    try:
+                        get_login_result(url, args.username, args.password)
+                    except RuntimeError:
+                        print("3秒后重试...")
+                        time.sleep(3)
+                        get_login_result(url, args.username, args.password)
+            else:
+                get_login_result(url, args.username, args.password)
 
 
 if __name__ == "__main__":
